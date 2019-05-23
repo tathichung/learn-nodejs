7ee84d74eae7c03ba0c999d7956f8d967ea4e9e6
Ta Thi Chung <tachung96.hust@gmail.com>
2019-05-23 23:52:46 +0700

    example for hello Chung

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+
 var port = 3000;
-app.get('/', function(request, response){
-    response.send('Hello word!');
+
+app.set('view engine', 'pug');
+app.set('views', './views')
+// app.get('/', function(req, res){
+//     res.send('Hello word!');
+// });
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello Chung</h1><a href="/users">User list</a>');
 });
+
+app.get('/users', function(req, res) {
+    res.send('User list');
+});
+
 app.listen(port, function() {
     console.log('Server listening on port' + port);
 });
